e4c99e9953c0b2890e8bcecf71e72407b7e309eb
louieiply <louieiply@gmail.com>
2022-08-07 23:01:24 +0930

    deploy third

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+// connection settings
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
